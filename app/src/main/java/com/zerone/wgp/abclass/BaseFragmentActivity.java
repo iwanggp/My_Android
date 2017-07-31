cca450a7fc43dcd5ce4d79436f63d8b53d7443d9
wanggp <gongpwang@qq.com>
2017-08-01 00:48:40 +0800

    Initial commit

---
+++ app/src/main/java/com/zerone/wgp/abclass/BaseFragmentActivity.java
@@ -0,0 +1,11 @@
+package com.zerone.wgp.abclass;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/8/1.
+ * 定义一个抽象的Activity这个Activity
+ */
+
+public class BaseFragmentActivity extends AppCompatActivity {
+}
