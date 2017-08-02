3e95086b4b45115be8d52151d18dba59c06eedac
Dev-wgp <gongpwang@qq.com>
2017-08-02 18:32:35 +0800

    branch in CKT ---CKT-wgp

---
+++ app/src/main/java/com/zerone/wgp/activities/HandlerDemoActivity.java
@@ -0,0 +1,77 @@
+package com.zerone.wgp.activities;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HandlerDemoActivity extends AppCompatActivity {
+	private TextView tv_handler;
+	private Button bt_handler;
+	//mHandler在主线程创建，所以自动绑定到主线程
+	private Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+				case 1:
+					System.out.println("handlermessage thread id is" + Thread.currentThread().getId());
+//					System.out.println();
+					System.out.println(msg.obj);
+					tv_handler.setText("完成ssssss");
+					break;
+			}
+		}
+	};
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_handler_demo);
+		tv_handler = (TextView) findViewById(R.id.tv_handler);
+		bt_handler = (Button) findViewById(R.id.bt_handler);
+		System.out.println("Main thread id is " + Thread.currentThread().getId());
+		bt_handler.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				DownLoader downLoader = new DownLoader();
+				downLoader.start();
+			}
+		});
+	}
+
+	private class DownLoader extends Thread {
+		@Override
+		public void run() {
+			System.out.println("开始下载文件.....");
+			try {
+				System.out.println("DownLoadThred id is " + Thread.currentThread().getId());
+				Thread.sleep(5000);
+				System.out.println("下载完成");
+				Message message = new Message();
+				message.what = 1;
+				message.arg1 = 1;
+				message.arg2 = 3;
+				message.obj = "ssss";
+
+				//下载完成后开始更新UI
+//				Runnable runnable = new Runnable() {
+//					@Override
+//					public void run() {
+//						System.out.println("Runable thread id " + Thread.currentThread().getId());
+//						tv_handler.setText("haoxiaoqian");
+//					}
+//				};
+//				mHandler.sendMessage(message);
+				mHandler.sendEmptyMessage(1);
+//				mHandler.post(runnable);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
--- app/src/main/java/com/zerone/wgp/fragment/ListNameFragment.java
+++ app/src/main/java/com/zerone/wgp/fragment/ListNameFragment.java
@@ -1,49 +1,82 @@
 package com.zerone.wgp.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.os.Handler;
+
 import android.support.v4.app.Fragment;
+
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lcodecore.tkrefreshlayout.RefreshListenerAdapter;
+import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
 import com.zerone.wgp.activities.R;
 import com.zerone.wgp.adapter.StudentRlvAdapter;
 import com.zerone.wgp.domain.Student;
+import com.zerone.wgp.utils.DividerItemDecoration;
 import com.zerone.wgp.utils.StudentLab;
 
 import java.util.List;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+import static com.zerone.wgp.activities.R.id.refreshLayout;
 
 /**
  * Created by Administrator on 2017/8/2.
  */
 
 public class ListNameFragment extends Fragment {
-    private List<Student> mStudentList;
-    private RecyclerView mRecyclerView;
-    private StudentRlvAdapter mStudentRlvAdapter;
+	private List<Student> mStudentList;
+	private RecyclerView mRecyclerView;
+	private StudentRlvAdapter mStudentRlvAdapter;
+	private TwinklingRefreshLayout mRefreshLayout;
 
-    public static ListNameFragment newInstance() {
-        return new ListNameFragment();
-    }
+	public static ListNameFragment newInstance() {
+		return new ListNameFragment();
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mStudentList = StudentLab.getStudentList();
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mStudentList = StudentLab.getStudentList();
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.list_name_fragment, container, false);
-        mRecyclerView = view.findViewById(R.id.list_student_rlv);//找到RecyclerView后要立马设置其布局
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-        mStudentRlvAdapter = new StudentRlvAdapter(getActivity(), mStudentList);
-        mRecyclerView.setAdapter(mStudentRlvAdapter);
-        return view;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.list_name_fragment, container, false);
+		mRefreshLayout = view.findViewById(R.id.refreshLayout);
+		mRecyclerView = view.findViewById(R.id.list_student_rlv);//找到RecyclerView后要立马设置其布局
+		mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+		mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
+		mRefreshLayout.setOnRefreshListener(new RefreshListenerAdapter() {
+			@Override
+			public void onRefresh(final TwinklingRefreshLayout refreshLayout) {
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+
+						refreshLayout.finishRefreshing();
+						mStudentList.add(new Student("haoxiaoqian", "454545"));
+					}
+				}, 2000);
+			}
+
+			@Override
+			public void onLoadMore(final TwinklingRefreshLayout refreshLayout) {
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+//						refreshLayout.finishLoadmore();
+						mStudentList.add(new Student("haoxiaoqian", "454545"));
+					}
+				}, 2000);
+			}
+		});
+		mStudentRlvAdapter = new StudentRlvAdapter(getActivity(), mStudentList);
+		mRecyclerView.setAdapter(mStudentRlvAdapter);
+		return view;
+	}
 }
